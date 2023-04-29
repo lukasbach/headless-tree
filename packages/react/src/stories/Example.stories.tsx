f19a72a4bf92f0c962b8ea13f568656e9b23a44b
Lukas Bach <contact@lukasbach.com>
2023-04-30 01:09:35 +0200

    storybook setup

---
+++ packages/react/src/stories/Example.stories.tsx
@@ -0,0 +1,10 @@
+import type { Meta } from "@storybook/react";
+import React from "react";
+
+const meta = {
+  title: "Example/Button",
+} satisfies Meta;
+
+export default meta;
+
+export const Example = () => <div>Hello</div>;
