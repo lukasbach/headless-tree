f340d2be8a465f893ea02dfab07a8291904327ef
Lukas Bach <contact@lukasbach.com>
2023-05-09 16:06:59 +0200

    storybook extract

---
+++ packages/sb-react/src/distinct-state-handlers.stories.tsx
@@ -0,0 +1,59 @@
+import type { Meta } from "@storybook/react";
+import React, { useState } from "react";
+import {
+  hotkeysCoreFeature,
+  selectionFeature,
+  syncDataLoaderFeature,
+} from "@headless-tree/core";
+import { useTree } from "@headless-tree/react";
+
+const meta = {
+  title: "React/Distinct State Handlers",
+} satisfies Meta;
+
+export default meta;
+
+export const DistinctStateHandlers = () => {
+  const [selectedItems, onChangeSelectedItems] = useState([]);
+  const [expandedItems, onChangeExpandedItems] = useState([]);
+  const [focusedItem, onChangeFocusedItem] = useState(null);
+
+  const tree = useTree<string>({
+    state: { selectedItems, expandedItems, focusedItem },
+    rootItemId: "root",
+    onChangeSelectedItems,
+    onChangeExpandedItems,
+    onChangeFocusedItem,
+    getItemName: (item) => item,
+    isItemFolder: () => true,
+    dataLoader: {
+      getItem: (itemId) => itemId,
+      getChildren: (itemId) => [`${itemId}-1`, `${itemId}-2`, `${itemId}-3`],
+    },
+    features: [syncDataLoaderFeature, selectionFeature, hotkeysCoreFeature],
+  });
+
+  return (
+    <div ref={tree.registerElement} className="tree">
+      {tree.getItems().map((item) => (
+        <div
+          key={item.getId()}
+          className="treeitem-parent"
+          style={{ marginLeft: `${item.getItemMeta().level * 20}px` }}
+        >
+          <button
+            {...item.getProps()}
+            ref={item.registerElement}
+            className="treeitem"
+            data-focused={item.isFocused()}
+            data-expanded={item.isExpanded()}
+            data-selected={item.isSelected()}
+          >
+            {item.isExpanded() ? "v " : "> "}
+            {item.getItemName()}
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
---
+++ packages/sb-react/src/search.stories.tsx
@@ -0,0 +1,74 @@
+import type { Meta } from "@storybook/react";
+import React from "react";
+import {
+  hotkeysCoreFeature,
+  selectionFeature,
+  syncDataLoaderFeature,
+  searchFeature,
+} from "@headless-tree/core";
+import { useTree } from "@headless-tree/react";
+
+const meta = {
+  title: "React/Search",
+} satisfies Meta;
+
+export default meta;
+
+export const Search = () => {
+  const tree = useTree<string>({
+    rootItemId: "root",
+    getItemName: (item) => item,
+    isItemFolder: () => true,
+    dataLoader: {
+      getItem: (itemId) => itemId,
+      getChildren: (itemId) => [`${itemId}-1`, `${itemId}-2`, `${itemId}-3`],
+    },
+    features: [
+      syncDataLoaderFeature,
+      selectionFeature,
+      hotkeysCoreFeature,
+      searchFeature,
+    ],
+  });
+
+  return (
+    <>
+      {tree.isSearchOpen() && (
+        <>
+          <input
+            {...tree.getSearchInputElementProps()}
+            ref={tree.registerSearchInputElement}
+          />{" "}
+          ({tree.getSearchMatchingItems().length} matches)
+        </>
+      )}{" "}
+      <div ref={tree.registerElement} className="tree">
+        {tree.getItems().map((item) => (
+          <div
+            key={item.getId()}
+            className="treeitem-parent"
+            style={{ marginLeft: `${item.getItemMeta().level * 20}px` }}
+          >
+            <button
+              {...item.getProps()}
+              ref={item.registerElement}
+              className="treeitem"
+              data-focused={item.isFocused()}
+              data-expanded={item.isExpanded()}
+              data-selected={item.isSelected()}
+              data-searchmatch={item.isMatchingSearch()}
+            >
+              {item.isExpanded() ? "v " : "> "}
+              {item.getItemName()}
+            </button>
+          </div>
+        ))}
+      </div>
+      <button onClick={() => tree.openSearch()}>Open Search</button> or press
+      any letter keys while focusing the tree to search.
+      {tree.isSearchOpen() && (
+        <p>Navigate between search results with ArrowUp and ArrowDown.</p>
+      )}
+    </>
+  );
+};
