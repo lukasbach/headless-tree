82ca2a848b8819286e91ed53f4c9a3b3a1b5d4dd
Lukas Bach <contact@lukasbach.com>
2023-05-09 15:52:49 +0200

    update types to simple interfaces

--- packages/docs/gatsby-config.ts
+++ packages/docs/gatsby-config.ts
@@ -3,7 +3,16 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
   // don't need to define it here (just if you need to change the options)
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/apidocs`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
   jsxRuntime: `automatic`,
 };
 
