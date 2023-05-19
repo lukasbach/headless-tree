345b2842d6fb770c0ef119edbaa106cb169b5929
Lukas Bach <contact@lukasbach.com>
2023-05-19 03:21:58 +0200

    main page

--- packages/docs/src/pages/index.tsx
+++ packages/docs/src/pages/index.tsx
@@ -1,15 +1,6 @@
 import React from "react";
-import Title from "../components/Title";
+import { LayoutContainer } from "@/components/layout/layout-container";
 
 export default function Home() {
-  return (
-    <main>
-      <Title>Hello TypeScript!</Title>
-      <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
-      <p>
-        Follow me on Twitter (
-        <a href="https://twitter.com/jpedroschmitz">@jpedroschmitz</a>)
-      </p>
-    </main>
-  );
+  return <LayoutContainer location="/">Yoo!</LayoutContainer>;
 }
