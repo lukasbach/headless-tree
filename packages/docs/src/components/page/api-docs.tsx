34504666fac7ab35d0d17bb9d407455f708e4e18
Lukas Bach <contact@lukasbach.com>
2023-08-03 03:08:47 +0200

    Revert "use DOMContentLoaded event"
    This reverts commit f20a22d94a1b26f609e8481b62d71f87baadb3ea.

--- packages/docs/src/components/page/api-docs.tsx
+++ packages/docs/src/components/page/api-docs.tsx
@@ -66,21 +66,19 @@ const DocsIframe: FC<{ src: string }> = ({ src }) => {
         height: "calc(100vh - var(--header-height))",
       }}
       onLoad={() => {
-        ref.current?.contentWindow?.addEventListener("DOMContentLoaded", () => {
-          if (!ref.current?.contentDocument || !ref.current.contentWindow) {
-            return;
-          }
-          ref.current.contentDocument.documentElement.setAttribute(
-            "data-theme",
-            theme.colorScheme
-          );
-          ref.current.contentDocument.body.style.background = "transparent";
-          ref.current.height = `${ref.current.contentWindow.document.body.scrollHeight}px`;
-          ref.current.contentDocument
-            .getElementsByClassName("tsd-page-toolbar")
-            .item(0)
-            ?.remove();
-        });
+        if (!ref.current?.contentDocument || !ref.current.contentWindow) {
+          return;
+        }
+        ref.current.contentDocument.documentElement.setAttribute(
+          "data-theme",
+          theme.colorScheme
+        );
+        ref.current.contentDocument.body.style.background = "transparent";
+        ref.current.height = `${ref.current.contentWindow.document.body.scrollHeight}px`;
+        ref.current.contentDocument
+          .getElementsByClassName("tsd-page-toolbar")
+          .item(0)
+          ?.remove();
       }}
     />
   );
