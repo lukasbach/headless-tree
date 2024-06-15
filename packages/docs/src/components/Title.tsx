2b42847553c2090fc62a5e3530ee72fa8f8267e2
Lukas Bach <contact@lukasbach.com>
2024-06-15 23:04:16 +0200

    remove old docs package

--- packages/docs/src/components/Title.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from "react";
-
-type TitleProps = {
-  children: ReactNode;
-};
-
-export default function Title({ children }: TitleProps) {
-  return <h1>{children}</h1>;
-}
--- packages/docs/src/pages/404.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function NotFound() {
-  return (
-    <main>
-      <p>Sorry, page not found!</p>
-    </main>
-  );
-}
