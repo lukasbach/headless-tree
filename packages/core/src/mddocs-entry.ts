c82afb3e89f50e79b8aadd1ee012e70fbbee3fe4
Lukas Bach <contact@lukasbach.com>
2024-06-15 20:25:52 +0200

    working feature state header

--- packages/core/src/mddocs-entry.ts
+++ packages/core/src/mddocs-entry.ts
@@ -1,17 +1,40 @@
 import { MainFeatureDef } from "./features/main/types";
 import { DragAndDropFeatureDef } from "./features/drag-and-drop/types";
+import { AsyncDataLoaderFeatureDef } from "./features/async-data-loader/types";
+import { ExpandAllFeatureDef } from "./features/expand-all/types";
+import { HotkeysCoreFeatureDef } from "./features/hotkeys-core/types";
+import { RenamingFeatureDef } from "./features/renaming/types";
+import { SearchFeatureDef } from "./features/search/types";
+import { SelectionFeatureDef } from "./features/selection/types";
+import { SyncDataLoaderFeatureDef } from "./features/sync-data-loader/types";
+import { TreeFeatureDef } from "./features/tree/types";
 
 export * from ".";
 
+//     /** @interface */
+//     export type XXXFeatureConfig<T> = XFeatureDef<T>["config"];
+//     /** @interface */
+//     export type XXXFeatureState<T> = XFeatureDef<T>["state"];
+//     /** @interface */
+//     export type XXXFeatureTreeInstance<T> = XFeatureDef<T>["treeInstance"];
+//     /** @interface */
+//     export type XXXFeatureItemInstance<T> = XFeatureDef<T>["itemInstance"];
+//     export type XXXFeatureHotkeys<T> = XFeatureDef<T>["hotkeys"];
+
 /** @interface */
-export type MainFeatureConfig = MainFeatureDef["config"];
+export type AsyncDataLoaderFeatureConfig<T> =
+  AsyncDataLoaderFeatureDef<T>["config"];
 /** @interface */
-export type MainFeatureState = MainFeatureDef["state"];
+export type AsyncDataLoaderFeatureState<T> =
+  AsyncDataLoaderFeatureDef<T>["state"];
 /** @interface */
-export type MainFeatureTreeInstance = MainFeatureDef["treeInstance"];
+export type AsyncDataLoaderFeatureTreeInstance<T> =
+  AsyncDataLoaderFeatureDef<T>["treeInstance"];
 /** @interface */
-export type MainFeatureItemInstance = MainFeatureDef["itemInstance"];
-export type MainFeatureHotkeys = MainFeatureDef["hotkeys"];
+export type AsyncDataLoaderFeatureItemInstance<T> =
+  AsyncDataLoaderFeatureDef<T>["itemInstance"];
+export type AsyncDataLoaderFeatureHotkeys<T> =
+  AsyncDataLoaderFeatureDef<T>["hotkeys"];
 
 /** @interface */
 export type DragAndDropFeatureConfig<T> = DragAndDropFeatureDef<T>["config"];
@@ -24,3 +47,94 @@ export type DragAndDropFeatureTreeInstance<T> =
 export type DragAndDropFeatureItemInstance<T> =
   DragAndDropFeatureDef<T>["itemInstance"];
 export type DragAndDropFeatureHotkeys<T> = DragAndDropFeatureDef<T>["hotkeys"];
+
+/** @interface */
+export type ExpandAllFeatureConfig = ExpandAllFeatureDef["config"];
+/** @interface */
+export type ExpandAllFeatureState = ExpandAllFeatureDef["state"];
+/** @interface */
+export type ExpandAllFeatureTreeInstance = ExpandAllFeatureDef["treeInstance"];
+/** @interface */
+export type ExpandAllFeatureItemInstance = ExpandAllFeatureDef["itemInstance"];
+export type ExpandAllFeatureHotkeys = ExpandAllFeatureDef["hotkeys"];
+
+/** @interface */
+export type HotkeysCoreFeatureConfig<T> = HotkeysCoreFeatureDef<T>["config"];
+/** @interface */
+export type HotkeysCoreFeatureState<T> = HotkeysCoreFeatureDef<T>["state"];
+/** @interface */
+export type HotkeysCoreFeatureTreeInstance<T> =
+  HotkeysCoreFeatureDef<T>["treeInstance"];
+/** @interface */
+export type HotkeysCoreFeatureItemInstance<T> =
+  HotkeysCoreFeatureDef<T>["itemInstance"];
+export type HotkeysCoreFeatureHotkeys<T> = HotkeysCoreFeatureDef<T>["hotkeys"];
+
+/** @interface */
+export type MainFeatureConfig = MainFeatureDef["config"];
+/** @interface */
+export type MainFeatureState = MainFeatureDef["state"];
+/** @interface */
+export type MainFeatureTreeInstance = MainFeatureDef["treeInstance"];
+/** @interface */
+export type MainFeatureItemInstance = MainFeatureDef["itemInstance"];
+export type MainFeatureHotkeys = MainFeatureDef["hotkeys"];
+
+/** @interface */
+export type RenamingFeatureConfig<T> = RenamingFeatureDef<T>["config"];
+/** @interface */
+export type RenamingFeatureState<T> = RenamingFeatureDef<T>["state"];
+/** @interface */
+export type RenamingFeatureTreeInstance<T> =
+  RenamingFeatureDef<T>["treeInstance"];
+/** @interface */
+export type RenamingFeatureItemInstance<T> =
+  RenamingFeatureDef<T>["itemInstance"];
+export type RenamingFeatureHotkeys<T> = RenamingFeatureDef<T>["hotkeys"];
+
+/** @interface */
+export type SearchFeatureConfig<T> = SearchFeatureDef<T>["config"];
+/** @interface */
+export type SearchFeatureState<T> = SearchFeatureDef<T>["state"];
+/** @interface */
+export type SearchFeatureTreeInstance<T> = SearchFeatureDef<T>["treeInstance"];
+/** @interface */
+export type SearchFeatureItemInstance<T> = SearchFeatureDef<T>["itemInstance"];
+export type SearchFeatureHotkeys<T> = SearchFeatureDef<T>["hotkeys"];
+
+/** @interface */
+export type SelectionFeatureConfig<T> = SelectionFeatureDef<T>["config"];
+/** @interface */
+export type SelectionFeatureState<T> = SelectionFeatureDef<T>["state"];
+/** @interface */
+export type SelectionFeatureTreeInstance<T> =
+  SelectionFeatureDef<T>["treeInstance"];
+/** @interface */
+export type SelectionFeatureItemInstance<T> =
+  SelectionFeatureDef<T>["itemInstance"];
+export type SelectionFeatureHotkeys<T> = SelectionFeatureDef<T>["hotkeys"];
+
+/** @interface */
+export type SyncDataLoaderFeatureConfig<T> =
+  SyncDataLoaderFeatureDef<T>["config"];
+/** @interface */
+export type SyncDataLoaderFeatureState<T> =
+  SyncDataLoaderFeatureDef<T>["state"];
+/** @interface */
+export type SyncDataLoaderFeatureTreeInstance<T> =
+  SyncDataLoaderFeatureDef<T>["treeInstance"];
+/** @interface */
+export type SyncDataLoaderFeatureItemInstance<T> =
+  SyncDataLoaderFeatureDef<T>["itemInstance"];
+export type SyncDataLoaderFeatureHotkeys<T> =
+  SyncDataLoaderFeatureDef<T>["hotkeys"];
+
+/** @interface */
+export type TreeFeatureConfig<T> = TreeFeatureDef<T>["config"];
+/** @interface */
+export type TreeFeatureState<T> = TreeFeatureDef<T>["state"];
+/** @interface */
+export type TreeFeatureTreeInstance<T> = TreeFeatureDef<T>["treeInstance"];
+/** @interface */
+export type TreeFeatureItemInstance<T> = TreeFeatureDef<T>["itemInstance"];
+export type TreeFeatureHotkeys<T> = TreeFeatureDef<T>["hotkeys"];
---
+++ packages/docs-new/src/components/docs-page/feature-page-header.tsx
@@ -0,0 +1,67 @@
+import { FC, PropsWithChildren } from "react";
+import { LinkRow } from "@site/src/components/docs-page/link-row";
+import { DocsPageHeader } from "@site/src/components/docs-page/docs-page-header";
+import styles from "./styles.module.css";
+
+const camelCase = (str: string) => {
+  return str.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
+};
+
+export const FeaturePageHeader: FC<
+  PropsWithChildren<{
+    title: string;
+    subtitle?: string;
+    feature: string;
+  }>
+> = ({ title, subtitle, children, feature }) => {
+  return (
+    <DocsPageHeader title={title} subtitle={subtitle}>
+      <LinkRow
+        link="View Source"
+        title="Source"
+        url={`https://github.com/lukasbach/headless-tree/blob/main/packages/core/src/features/${feature}/feature.ts`}
+      />
+      <LinkRow
+        link="View Types"
+        title="Types"
+        url={`https://github.com/lukasbach/headless-tree/blob/main/packages/core/src/features/${feature}/types.ts`}
+      />
+      <tr>
+        <td>Import</td>
+        <td className={styles.code}>
+          import {`{ ${feature}Feature } from "@headless-tree/core`}
+        </td>
+      </tr>
+      <tr>
+        <td>Type Documentation</td>
+        <td>
+          <a
+            href={`/api/core/interface/${camelCase(feature)}FeatureConfig`}
+            className={styles.typedocLink}
+          >
+            Configuration
+          </a>
+          <a
+            href={`/api/core/interface/${camelCase(feature)}FeatureState`}
+            className={styles.typedocLink}
+          >
+            State
+          </a>
+          <a
+            href={`/api/core/interface/${camelCase(feature)}FeatureTreeInstance`}
+            className={styles.typedocLink}
+          >
+            Tree Instance
+          </a>
+          <a
+            href={`/api/core/interface/${camelCase(feature)}FeatureItemInstance`}
+            className={styles.typedocLink}
+          >
+            Item Instance
+          </a>
+        </td>
+      </tr>
+      {children}
+    </DocsPageHeader>
+  );
+};
