5b45c618741ab10bb397e4932b8cc3f97b503c43
Lukas Bach <contact@lukasbach.com>
2024-06-15 21:01:15 +0200

    basic start page

--- packages/docs-new/docusaurus.config.ts
+++ packages/docs-new/docusaurus.config.ts
@@ -5,20 +5,14 @@ import path from "path";
 import { storybookPlugin } from "./storybook-plugin";
 
 const config: Config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "Headless Tree",
+  tagline: "Spiritual Successor to react-complex-tree. Work In Progress.",
   favicon: "img/favicon.ico",
 
-  // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: "https://headless-tree.lukasbach.com",
   baseUrl: "/",
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "lukasbach",
+  projectName: "headless-tree",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
@@ -57,17 +51,13 @@ const config: Config = {
         docs: {
           sidebarCollapsible: false,
           sidebarPath: "./sidebars.ts",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/lukasbach/headless-tree/tree/main/packages/docs/",
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/lukasbach/headless-tree/tree/main/packages/docs/",
         },
         theme: {
           customCss: "./src/css/custom.css",
@@ -80,11 +70,7 @@ const config: Config = {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: "My Site",
-      logo: {
-        alt: "My Site Logo",
-        src: "img/logo.svg",
-      },
+      title: "Headless Tree",
       items: [
         {
           type: "docSidebar",
@@ -98,7 +84,7 @@ const config: Config = {
           position: "left",
         },
         {
-          href: "https://github.com/facebook/docusaurus",
+          href: "https://github.com/lukasbach/headless-tree",
           label: "GitHub",
           position: "right",
         },
