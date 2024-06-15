06af5dcdc6e581d0d0a60bfbb529fd099522dafb
Lukas Bach <contact@lukasbach.com>
2024-06-15 22:40:32 +0200

    fix all stories page

--- packages/docs-new/src/util/use-all-stories.ts
+++ packages/docs-new/src/util/use-all-stories.ts
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useStories } from "@site/src/util/use-stories";
 
-// TODO not properly working yet, only when editing this file, hotreload fixes the ui. too many downloads at once?
 export const useAllStories = (skip?: boolean) => {
   const [storyList, setStoryList] = useState<string[] | null>(null);
 
