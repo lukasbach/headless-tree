e983097ee86a170e82087a07a1544e9aaaf3971a
Lukas Bach <contact@lukasbach.com>
2024-06-21 23:41:35 +0200

    fix feature order

--- packages/core/src/core/build-static-instance.ts
+++ packages/core/src/core/build-static-instance.ts
@@ -10,17 +10,17 @@ export const buildStaticInstance: InstanceBuilder = (
   const instance = {};
   const finalize = () => {
     const opts = buildOpts(instance);
-    featureLoop: for (let i = features.length - 1; i >= 0; i--) {
+    featureLoop: for (let i = 0; i < features.length; i++) {
+      // Loop goes in forward order, because later features overwrite previous ones
+      // TODO loop order correct? I think so...
       const definition = features[i][instanceType];
       if (!definition) continue featureLoop;
       methodLoop: for (const [key, method] of Object.entries(definition)) {
         if (!method) continue methodLoop;
-        if (method) {
-          const prev = instance[key];
-          instance[key] = (...args) => {
-            return method({ ...opts, prev }, ...args);
-          };
-        }
+        const prev = instance[key];
+        instance[key] = (...args) => {
+          return method({ ...opts, prev }, ...args);
+        };
       }
     }
   };
