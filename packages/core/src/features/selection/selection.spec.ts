bc8df71476c94738b49924399b4e3236225e27c4
Lukas Bach <contact@lukasbach.com>
2025-01-24 23:23:43 +0100

    finalize selection tests

--- packages/core/src/features/selection/selection.spec.ts
+++ packages/core/src/features/selection/selection.spec.ts
@@ -1,67 +1,220 @@
 import { describe, expect, it } from "vitest";
 import { TestTree } from "../../test-utils/test-tree";
-import { syncDataLoaderFeature } from "../sync-data-loader/feature";
 import { selectionFeature } from "./feature";
 
+const factory = TestTree.default({}).withFeatures(selectionFeature);
+
+// it("test", async () => {
+//   const tree = await factory.suits.async().tree.createDebugTree();
+//   const setSelectedItems = tree.mockedHandler("setSelectedItems");
+//   const setFocusedItem = tree.mockedHandler("setFocusedItem");
+//
+//   tree.do.selectItem("x111");
+//   tree.do.ctrlSelectItem("x112");
+//   tree.do.ctrlSelectItem("x113");
+//
+//   expect(setSelectedItems).toHaveBeenCalledWith(["x111", "x112", "x113"]);
+//   expect(setFocusedItem).toHaveBeenCalledWith("x113");
+// });
+
 describe("core-feature/selections", () => {
-  // describe.for([syncTree, asyncTree, proxifiedSyncTree, proxifiedAsyncTree], () => {});
-  const tree = TestTree.default({})
-    .withFeatures(syncDataLoaderFeature, selectionFeature)
-    .withResetBeforeEach();
+  factory.forSuits((tree) => {
+    it("sets aria-selected to false", () => {
+      expect(
+        tree.instance.getItemInstance("x111").getProps()["aria-selected"],
+      ).toBe("false");
+    });
 
-  it("sets aria-selected to false", () => {
-    expect(
-      tree.instance.getItemInstance("x111").getProps()["aria-selected"],
-    ).toBe("false");
-  });
-
-  it("sets aria-selected to true", () => {
-    tree.do.selectItem("x111");
-    expect(
-      tree.instance.getItemInstance("x111").getProps()["aria-selected"],
-    ).toBe("true");
-  });
-
-  describe("select and control select", () => {
-    it("should make isSelected true", () => {
+    it("sets aria-selected to true", () => {
       tree.do.selectItem("x111");
-      tree.do.ctrlSelectItem("x112");
-      tree.do.ctrlSelectItem("x113");
+      expect(
+        tree.instance.getItemInstance("x111").getProps()["aria-selected"],
+      ).toBe("true");
+    });
 
-      expect(tree.instance.getItemInstance("x111").isSelected()).toBe(true);
+    it("resets old selection after new select", () => {
+      tree.do.selectItem("x111");
+      tree.do.selectItem("x112");
+      expect(
+        tree.instance.getItemInstance("x111").getProps()["aria-selected"],
+      ).toBe("false");
+      expect(
+        tree.instance.getItemInstance("x112").getProps()["aria-selected"],
+      ).toBe("true");
+      expect(tree.instance.getItemInstance("x111").isSelected()).toBe(false);
       expect(tree.instance.getItemInstance("x112").isSelected()).toBe(true);
-      expect(tree.instance.getItemInstance("x113").isSelected()).toBe(true);
     });
 
-    it("should call individual state setters", () => {
-      const setSelectedItems = tree.mockedHandler("setSelectedItems");
-      const setFocusedItem = tree.mockedHandler("setFocusedItem");
+    describe("select and control select", () => {
+      it("should make isSelected true", () => {
+        tree.do.selectItem("x111");
+        tree.do.ctrlSelectItem("x112");
+        tree.do.ctrlSelectItem("x113");
 
-      tree.do.selectItem("x111");
-      tree.do.ctrlSelectItem("x112");
-      tree.do.ctrlSelectItem("x113");
+        expect(tree.instance.getItemInstance("x111").isSelected()).toBe(true);
+        expect(tree.instance.getItemInstance("x112").isSelected()).toBe(true);
+        expect(tree.instance.getItemInstance("x113").isSelected()).toBe(true);
+      });
 
-      expect(setSelectedItems).toHaveBeenCalledWith(["x111", "x112", "x113"]);
-      expect(setFocusedItem).toHaveBeenCalledWith("x113");
+      it("should call individual state setters", () => {
+        const setSelectedItems = tree.mockedHandler("setSelectedItems");
+        const setFocusedItem = tree.mockedHandler("setFocusedItem");
+
+        tree.do.selectItem("x111");
+        tree.do.ctrlSelectItem("x112");
+        tree.do.ctrlSelectItem("x113");
+
+        expect(setSelectedItems).toHaveBeenCalledWith(["x111", "x112", "x113"]);
+        expect(setFocusedItem).toHaveBeenCalledWith("x113");
+      });
+
+      it("should call joint state setter", () => {
+        const setState = tree.mockedHandler("setState");
+
+        tree.do.selectItem("x111");
+        tree.do.ctrlSelectItem("x112");
+        tree.do.ctrlSelectItem("x113");
+
+        expect(setState).toHaveBeenCalledWith(
+          expect.objectContaining({
+            selectedItems: ["x111", "x112", "x113"],
+            focusedItem: "x113",
+          }),
+        );
+      });
     });
 
-    it("should call joint state setter", () => {
-      const setState = tree.mockedHandler("setState");
+    describe("shift select", () => {
+      it("should make isSelected true", () => {
+        tree.do.selectItem("x111");
+        tree.do.shiftSelectItem("x113");
+        tree.do.ctrlSelectItem("x2");
 
-      tree.do.selectItem("x111");
-      tree.do.ctrlSelectItem("x112");
-      tree.do.ctrlSelectItem("x113");
+        expect(tree.instance.getItemInstance("x111").isSelected()).toBe(true);
+        expect(tree.instance.getItemInstance("x112").isSelected()).toBe(true);
+        expect(tree.instance.getItemInstance("x113").isSelected()).toBe(true);
+        expect(tree.instance.getItemInstance("x2").isSelected()).toBe(true);
+      });
 
-      expect(setState).toHaveBeenCalledWith(
-        expect.objectContaining({
-          selectedItems: ["x111", "x112", "x113"],
-          focusedItem: "x113",
-        }),
-      );
+      it("should call individual state setters", () => {
+        const setSelectedItems = tree.mockedHandler("setSelectedItems");
+        const setFocusedItem = tree.mockedHandler("setFocusedItem");
+
+        tree.do.selectItem("x111");
+        tree.do.shiftSelectItem("x113");
+        expect(setFocusedItem).toHaveBeenCalledWith("x113");
+
+        tree.do.ctrlSelectItem("x2");
+
+        expect(setSelectedItems).toHaveBeenCalledWith([
+          "x111",
+          "x112",
+          "x113",
+          "x2",
+        ]);
+
+        expect(setFocusedItem).toHaveBeenCalledWith("x2");
+      });
+
+      it("should call joint state setter", () => {
+        const setState = tree.mockedHandler("setState");
+
+        tree.do.selectItem("x111");
+        tree.do.shiftSelectItem("x113");
+        expect(setState).toHaveBeenCalledWith(
+          expect.objectContaining({
+            selectedItems: ["x111", "x112", "x113"],
+            focusedItem: "x113",
+          }),
+        );
+
+        tree.do.ctrlSelectItem("x2");
+
+        expect(setState).toHaveBeenCalledWith(
+          expect.objectContaining({
+            selectedItems: ["x111", "x112", "x113", "x2"],
+            focusedItem: "x2",
+          }),
+        );
+      });
+    });
+
+    describe("programmatic select", () => {
+      describe("item instance actions", () => {
+        it("should handle select", () => {
+          const setSelectedItems = tree.mockedHandler("setSelectedItems");
+          tree.instance.getItemInstance("x111").select();
+          expect(setSelectedItems).toHaveBeenCalledWith(["x111"]);
+          expect(tree.instance.getItemInstance("x111").isSelected()).toBe(true);
+        });
+
+        it("should handle deselect", () => {
+          const setSelectedItems = tree.mockedHandler("setSelectedItems");
+          tree.instance.getItemInstance("x111").select();
+          tree.instance.getItemInstance("x111").deselect();
+          expect(setSelectedItems).toHaveBeenCalledWith([]);
+          expect(tree.instance.getItemInstance("x111").isSelected()).toBe(
+            false,
+          );
+        });
+
+        it("should handle toggle select on", () => {
+          const setSelectedItems = tree.mockedHandler("setSelectedItems");
+          tree.instance.getItemInstance("x111").toggleSelect();
+          expect(setSelectedItems).toHaveBeenCalledWith(["x111"]);
+          expect(tree.instance.getItemInstance("x111").isSelected()).toBe(true);
+        });
+
+        it("should handle toggle select off", () => {
+          const setSelectedItems = tree.mockedHandler("setSelectedItems");
+          tree.instance.getItemInstance("x111").select();
+          tree.instance.getItemInstance("x111").toggleSelect();
+          expect(setSelectedItems).toHaveBeenCalledWith([]);
+          expect(tree.instance.getItemInstance("x111").isSelected()).toBe(
+            false,
+          );
+        });
+
+        it("should handle selectUpTo without ctrl", () => {
+          const setSelectedItems = tree.mockedHandler("setSelectedItems");
+          tree.instance.getItemInstance("x111").toggleSelect();
+          tree.instance.getItemInstance("x112").toggleSelect();
+          tree.instance.getItemInstance("x112").setFocused();
+          tree.instance.getItemInstance("x114").selectUpTo(false);
+          expect(setSelectedItems).toHaveBeenCalledWith([
+            "x112",
+            "x113",
+            "x114",
+          ]);
+          expect(tree.instance.getItemInstance("x112").isSelected()).toBe(true);
+        });
+
+        it("should handle selectUpTo with ctrl", () => {
+          const setSelectedItems = tree.mockedHandler("setSelectedItems");
+          tree.instance.getItemInstance("x111").toggleSelect();
+          tree.instance.getItemInstance("x112").toggleSelect();
+          tree.instance.getItemInstance("x112").setFocused();
+          tree.instance.getItemInstance("x114").selectUpTo(true);
+          expect(setSelectedItems).toHaveBeenCalledWith([
+            "x111",
+            "x112",
+            "x113",
+            "x114",
+          ]);
+          expect(tree.instance.getItemInstance("x112").isSelected()).toBe(true);
+        });
+      });
+
+      it("should handle getters", () => {
+        const setSelectedItems = tree.mockedHandler("setSelectedItems");
+        tree.instance.setSelectedItems(["x111", "x112"]);
+        expect(setSelectedItems).toHaveBeenCalledWith(["x111", "x112"]);
+        expect(tree.instance.getItemInstance("x111").isSelected()).toBe(true);
+        expect(tree.instance.getItemInstance("x112").isSelected()).toBe(true);
+        expect(tree.instance.getSelectedItems()[0].getId()).toEqual("x111");
+        expect(tree.instance.getSelectedItems()[1].getId()).toEqual("x112");
+        expect(tree.instance.getSelectedItems().length).toBe(2);
+      });
     });
   });
-
-  describe("shift select", () => {});
-  describe("resets old selection after new select", () => {});
-  describe("programmatic select", () => {});
 });
--- packages/core/src/test-utils/test-tree.ts
+++ packages/core/src/test-utils/test-tree.ts
@@ -1,8 +1,11 @@
-import { beforeEach, vi } from "vitest";
+import { beforeEach, describe, vi } from "vitest";
 import { TreeConfig, TreeInstance } from "../types/core";
 import { createTree } from "../core/create-tree";
 import { TestTreeDo } from "./test-tree-do";
 import { TestTreeExpect } from "./test-tree-expect";
+import { syncDataLoaderFeature } from "../features/sync-data-loader/feature";
+import { asyncDataLoaderFeature } from "../features/async-data-loader/feature";
+import { buildProxiedInstance } from "../core/build-proxified-instance";
 
 export class TestTree<T = string> {
   public readonly do = new TestTreeDo(this);
@@ -10,6 +13,43 @@ export class TestTree<T = string> {
   public readonly expect = new TestTreeExpect(this);
 
   private treeInstance: TreeInstance<T> | null = null;
+
+  private static asyncLoaderResolvers: (() => void)[] = [];
+
+  suits = {
+    sync: () => ({
+      tree: this.withFeatures(syncDataLoaderFeature),
+      title: "Synchronous Data Loader",
+    }),
+    async: () => ({
+      tree: this.withFeatures(asyncDataLoaderFeature),
+      title: "Asynchronous Data Loader",
+    }),
+    proxifiedSync: () => ({
+      tree: this.withFeatures(syncDataLoaderFeature).with({
+        instanceBuilder: buildProxiedInstance,
+      }),
+      title: "Proxified Synchronous Data Loader",
+    }),
+    proxifiedAsync: () => ({
+      tree: this.withFeatures(asyncDataLoaderFeature).with({
+        instanceBuilder: buildProxiedInstance,
+      }),
+      title: "Proxified Asynchronous Data Loader",
+    }),
+  };
+
+  forSuits(runSuite: (tree: TestTree<T>) => void) {
+    describe.for([
+      this.suits.sync(),
+      this.suits.async(),
+      this.suits.proxifiedSync(),
+      this.suits.proxifiedAsync(),
+    ])("$title", ({ tree }) => {
+      tree.resetBeforeEach();
+      runSuite(tree);
+    });
+  }
 
   get instance() {
     if (!this.treeInstance) {
@@ -20,16 +60,40 @@ export class TestTree<T = string> {
 
   private constructor(private config: TreeConfig<T>) {}
 
+  static async resolveAsyncLoaders() {
+    while (TestTree.asyncLoaderResolvers.length) {
+      TestTree.asyncLoaderResolvers.shift()?.();
+      await new Promise<void>((r) => {
+        setTimeout(r);
+      });
+    }
+  }
+
   static default(config: Partial<TreeConfig<string>>) {
     return new TestTree({
       rootItemId: "x",
+      createLoadingItemData: () => "loading",
       dataLoader: {
         getItem: (id) => id,
-        getChildren: (id) => [`${id}1`, `${id}2`, `${id}3`, ` ${id}4`],
+        getChildren: (id) => [`${id}1`, `${id}2`, `${id}3`, `${id}4`],
+      },
+      asyncDataLoader: {
+        getItem: async (id) => {
+          await new Promise<void>((r) => {
+            TestTree.asyncLoaderResolvers.push(r);
+          });
+          return id;
+        },
+        getChildren: async (id) => {
+          await new Promise<void>((r) => {
+            TestTree.asyncLoaderResolvers.push(r);
+          });
+          return [`${id}1`, `${id}2`, `${id}3`, `${id}4`];
+        },
       },
       getItemName: (item) => item.getItemData(),
       indent: 20,
-      isItemFolder: (item) => item.getItemMeta().level < 3, // TODO?
+      isItemFolder: (item) => item.getItemMeta().level < 2,
       initialState: {
         expandedItems: ["x1", "x11"],
       },
@@ -37,25 +101,68 @@ export class TestTree<T = string> {
     });
   }
 
-  withResetBeforeEach() {
-    beforeEach(() => {
+  with(config: Partial<TreeConfig<T>>) {
+    return new TestTree({ ...this.config, ...config });
+  }
+
+  resetBeforeEach() {
+    beforeEach(async () => {
       this.reset();
+      vi.clearAllMocks();
+      // trigger instance creation
+      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+      this.instance;
+      await TestTree.resolveAsyncLoaders();
     });
+  }
+
+  async createDebugTree() {
+    this.reset();
+    vi.clearAllMocks();
+    // trigger instance creation
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    this.instance;
+    await TestTree.resolveAsyncLoaders();
     return this;
   }
 
   withFeatures(...features: any) {
-    this.config.features ??= [];
-    this.config.features.push(...features);
-    return this;
+    return this.with({
+      features: [...(this.config.features ?? []), ...features],
+    });
   }
 
   mockedHandler(handlerName: keyof TreeConfig<T>) {
-    this.config[handlerName as any] = vi.fn();
-    return this.config[handlerName];
+    const mock = vi.fn();
+    if (this.treeInstance) {
+      this.treeInstance?.setConfig((prev) => ({
+        ...prev,
+        [handlerName as any]: mock,
+      }));
+    } else {
+      this.config[handlerName as any] = mock;
+    }
+    return mock;
   }
 
   reset() {
     this.treeInstance = null;
+    TestTree.asyncLoaderResolvers = [];
+  }
+
+  debug() {
+    console.log(
+      this.instance
+        .getItems()
+        .map((item) =>
+          [
+            "  ".repeat(item.getItemMeta().level),
+            '"',
+            item.getItemMeta().itemId,
+            '"',
+          ].join(""),
+        )
+        .join("\n"),
+    );
   }
 }
