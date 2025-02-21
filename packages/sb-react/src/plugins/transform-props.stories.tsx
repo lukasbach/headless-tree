4370f7934d7ceff5410aacd40dac240815f632c0
Lukas Bach <contact@lukasbach.com>
2025-02-21 17:51:36 +0100

    story for transforming props

---
+++ packages/sb-react/src/plugins/transform-props.stories.tsx
@@ -0,0 +1,75 @@
+import type { Meta } from "@storybook/react";
+import React from "react";
+import {
+  FeatureImplementation,
+  hotkeysCoreFeature,
+  selectionFeature,
+  syncDataLoaderFeature,
+} from "@headless-tree/core";
+import { useTree } from "@headless-tree/react";
+import cx from "classnames";
+
+const meta = {
+  title: "React/Plugins/Transform Props",
+  tags: ["guide/plugin", "basic"],
+} satisfies Meta;
+
+export default meta;
+
+// story-start
+const customFeature: FeatureImplementation = {
+  itemInstance: {
+    getProps: ({ prev }) => {
+      const { "aria-label": label, ...props } = prev?.() ?? {};
+      return { ...props, "data-label": label };
+    },
+  },
+};
+
+export const TransformProps = () => {
+  const tree = useTree<string>({
+    rootItemId: "folder",
+    getItemName: (item) => item.getItemData(),
+    isItemFolder: (item) => !item.getItemData().endsWith("item"),
+    indent: 20,
+    dataLoader: {
+      getItem: (itemId) => itemId,
+      getChildren: (itemId) => [
+        `${itemId}-1`,
+        `${itemId}-2`,
+        `${itemId}-3`,
+        `${itemId}-1item`,
+        `${itemId}-2item`,
+      ],
+    },
+    features: [
+      syncDataLoaderFeature,
+      selectionFeature,
+      hotkeysCoreFeature,
+      customFeature,
+    ],
+  });
+
+  return (
+    <div {...tree.getContainerProps()} className="tree">
+      {tree.getItems().map((item) => (
+        <button
+          {...item.getProps()}
+          key={item.getId()}
+          style={{ paddingLeft: `${item.getItemMeta().level * 20}px` }}
+        >
+          <div
+            className={cx("treeitem", {
+              focused: item.isFocused(),
+              expanded: item.isExpanded(),
+              selected: item.isSelected(),
+              folder: item.isFolder(),
+            })}
+          >
+            {item.getItemName()}
+          </div>
+        </button>
+      ))}
+    </div>
+  );
+};
