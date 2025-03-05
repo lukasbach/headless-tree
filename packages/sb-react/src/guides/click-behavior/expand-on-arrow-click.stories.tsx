42ecd828432cbd5eac15928d3bea0038536d63d4
Lukas Bach <contact@lukasbach.com>
2025-03-05 01:40:57 +0100

    click behavior guides

---
+++ packages/sb-react/src/guides/click-behavior/expand-on-arrow-click.stories.tsx
@@ -0,0 +1,100 @@
+import type { Meta } from "@storybook/react";
+import React from "react";
+import {
+  FeatureImplementation,
+  dragAndDropFeature,
+  hotkeysCoreFeature,
+  selectionFeature,
+  syncDataLoaderFeature,
+} from "@headless-tree/core";
+import { useTree } from "@headless-tree/react";
+import cx from "classnames";
+
+import "./expand-on-arrow-click.css";
+
+const meta = {
+  title: "React/Guides/Click Behavior/Expand On Arrow Click",
+  tags: ["guide/clickbehavior", "basic"],
+} satisfies Meta;
+
+export default meta;
+
+// story-start
+
+const customClickBehavior: FeatureImplementation = {
+  itemInstance: {
+    getProps: ({ tree, item, prev }) => ({
+      ...prev?.(),
+      onClick: (e: MouseEvent) => {
+        if (e.shiftKey) {
+          item.selectUpTo(e.ctrlKey || e.metaKey);
+        } else if (e.ctrlKey || e.metaKey) {
+          item.toggleSelect();
+        } else {
+          tree.setSelectedItems([item.getItemMeta().itemId]);
+        }
+
+        item.setFocused();
+      },
+    }),
+  },
+};
+
+export const ExpandOnArrowClick = () => {
+  const tree = useTree<string>({
+    rootItemId: "folder",
+    getItemName: (item) => item.getItemData(),
+    isItemFolder: (item) => !item.getItemData().endsWith("item"),
+    indent: 20,
+    dataLoader: {
+      getItem: (itemId) => itemId,
+      getChildren: (itemId) => [
+        `${itemId}-1`,
+        `${itemId}-2`,
+        `${itemId}-3`,
+        `${itemId}-1item`,
+        `${itemId}-2item`,
+      ],
+    },
+    onDrop: console.log,
+    features: [
+      syncDataLoaderFeature,
+      selectionFeature,
+      dragAndDropFeature,
+      hotkeysCoreFeature,
+      customClickBehavior,
+    ],
+  });
+
+  return (
+    <div {...tree.getContainerProps()} className="tree">
+      {tree.getItems().map((item) => (
+        <div
+          className="outeritem"
+          key={item.getId()}
+          style={{ paddingLeft: `${item.getItemMeta().level * 20}px` }}
+        >
+          {item.isFolder() && (
+            <button onClick={item.isExpanded() ? item.collapse : item.expand}>
+              {item.isExpanded() ? "v" : ">"}
+            </button>
+          )}
+          <button {...item.getProps()} key={item.getId()}>
+            <div
+              className={cx("treeitem", {
+                focused: item.isFocused(),
+                expanded: item.isExpanded(),
+                selected: item.isSelected(),
+                folder: item.isFolder(),
+                drop: item.isDropTarget(),
+              })}
+            >
+              {item.getItemName()}
+            </div>
+          </button>
+        </div>
+      ))}
+      <div style={tree.getDragLineStyle()} className="dragline" />
+    </div>
+  );
+};
---
+++ packages/sb-react/src/guides/click-behavior/expand-on-double-click.stories.tsx
@@ -0,0 +1,106 @@
+import type { Meta } from "@storybook/react";
+import React from "react";
+import {
+  FeatureImplementation,
+  dragAndDropFeature,
+  hotkeysCoreFeature,
+  selectionFeature,
+  syncDataLoaderFeature,
+} from "@headless-tree/core";
+import { useTree } from "@headless-tree/react";
+import cx from "classnames";
+
+const meta = {
+  title: "React/Guides/Click Behavior/Expand On Double Click",
+  tags: ["guide/clickbehavior", "basic"],
+} satisfies Meta;
+
+export default meta;
+
+// story-start
+
+const customClickBehavior: FeatureImplementation = {
+  itemInstance: {
+    getProps: ({ tree, item, prev }) => ({
+      ...prev?.(),
+      onDoubleClick: (e: MouseEvent) => {
+        item.primaryAction();
+
+        if (!item.isFolder()) {
+          return;
+        }
+
+        if (item.isExpanded()) {
+          item.collapse();
+        } else {
+          item.expand();
+        }
+      },
+      onClick: (e: MouseEvent) => {
+        if (e.shiftKey) {
+          item.selectUpTo(e.ctrlKey || e.metaKey);
+        } else if (e.ctrlKey || e.metaKey) {
+          item.toggleSelect();
+        } else {
+          tree.setSelectedItems([item.getItemMeta().itemId]);
+        }
+
+        item.setFocused();
+      },
+    }),
+  },
+};
+
+export const ExpandOnDoubleClick = () => {
+  const tree = useTree<string>({
+    rootItemId: "folder",
+    getItemName: (item) => item.getItemData(),
+    isItemFolder: (item) => !item.getItemData().endsWith("item"),
+    indent: 20,
+    dataLoader: {
+      getItem: (itemId) => itemId,
+      getChildren: (itemId) => [
+        `${itemId}-1`,
+        `${itemId}-2`,
+        `${itemId}-3`,
+        `${itemId}-1item`,
+        `${itemId}-2item`,
+      ],
+    },
+    onDrop: console.log,
+    features: [
+      syncDataLoaderFeature,
+      selectionFeature,
+      dragAndDropFeature,
+      hotkeysCoreFeature,
+      customClickBehavior,
+    ],
+  });
+
+  return (
+    <div {...tree.getContainerProps()} className="tree">
+      {tree.getItems().map((item) => (
+        <div className="outeritem" key={item.getId()}>
+          <button
+            {...item.getProps()}
+            key={item.getId()}
+            style={{ paddingLeft: `${item.getItemMeta().level * 20}px` }}
+          >
+            <div
+              className={cx("treeitem", {
+                focused: item.isFocused(),
+                expanded: item.isExpanded(),
+                selected: item.isSelected(),
+                folder: item.isFolder(),
+                drop: item.isDropTarget(),
+              })}
+            >
+              {item.getItemName()}
+            </div>
+          </button>
+        </div>
+      ))}
+      <div style={tree.getDragLineStyle()} className="dragline" />
+    </div>
+  );
+};
