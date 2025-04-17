077b37de19f8d26c86c5a888bf2de8e2f195e0cf
Lukas Bach <contact@lukasbach.com>
2025-04-17 16:48:32 +0200

    docs: fix social banner

--- packages/docs/docusaurus.config.ts
+++ packages/docs/docusaurus.config.ts
@@ -69,7 +69,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/banner-1.png",
     navbar: {
       logo: {
         alt: "Site Logo",
