e47a735112d9db606320d16f84ee773a7cd1731e
Lukas Bach <contact@lukasbach.com>
2025-04-18 02:17:14 +0200

    feat: starting checkboxes

---
+++ packages/core/src/features/checkboxes/types.ts
@@ -0,0 +1,36 @@
+import { SetStateFn } from "../../types/core";
+
+export type CheckboxesState = {
+  checkedItems?: string[];
+  uncheckedItems?: string[];
+  checkedFolders?: string[];
+  indeterminates?: string[];
+};
+
+export enum CheckedState {
+  Checked = "checked",
+  Unchecked = "unchecked",
+  Indeterminate = "indeterminate",
+}
+
+export type CheckboxesFeatureDef<T> = {
+  state: {
+    checkedState: CheckboxesState;
+  };
+  config: {
+    setCheckedState?: SetStateFn<CheckboxesState>;
+    inheritCheckedState?: boolean;
+  };
+  treeInstance: {
+    setCheckedState: (checkedState: CheckboxesState) => void;
+    getCheckedItemIds: () => string[];
+  };
+  itemInstance: {
+    setChecked: (inherit?: boolean) => void;
+    setUnchecked: (inherit?: boolean) => void;
+    toggleCheckedState: () => void;
+    getCheckedState: () => CheckedState;
+    getCheckboxProps: () => Record<string, any>;
+  };
+  hotkeys: never;
+};
