ce56635d2bb293ea3cc139410c341c5a1f073cfb
Lukas Bach <contact@lukasbach.com>
2025-04-26 18:32:47 +0200

    basic example code

--- scripts/prepare.mjs
+++ scripts/prepare.mjs
@@ -2,6 +2,7 @@
 
 import 'zx/globals';
 
+// ------ CHANGELOG COPYING ------
 const copyChangelog = async (pkg) => {
     const contents = await fs.readFile(path.join(__dirname, `../packages/${pkg}/CHANGELOG.md`));
     const prefix = [
@@ -20,3 +21,29 @@ await fs.copy(
     path.join(__dirname, `../CONTRIBUTING.md`),
     path.join(__dirname, `../packages/docs/docs/contributing/1-overview.mdx`)
 );
+
+
+// ------ EXAMPLES REDUNDANT FILES COPYING ------
+const samples = await glob('./examples/*/package.json');
+
+for (const sample of samples) {
+    const sampleName = /\.\/examples\/(.*)\/package.json/.exec(sample)[1];
+    await fs.copy(
+        path.join(__dirname, "../packages/sb-react/.storybook/style.css"),
+        path.join(__dirname, "../examples", sampleName, "src/style.css")
+    );
+    await fs.copy(
+        path.join(__dirname, "examples-data-template.ts"),
+        path.join(__dirname, "../examples", sampleName, "src/data.ts")
+    );
+
+    const stackblitz = `https://stackblitz.com/github/lukasbach/headless-tree/tree/main/examples/${sampleName}?embed=1&theme=dark&preset=node&file=src/main.tsx`;
+    const codesandbox = `https://codesandbox.io/p/devbox/github/lukasbach/headless-tree/tree/main/examples/${sampleName}?embed=1&theme=dark&file=src/main.tsx`;
+    const github = `https://github.com/lukasbach/headless-tree/tree/main/examples/${sampleName}`;
+    await fs.writeFile(
+        path.join(__dirname, `../examples/${sampleName}/readme.MD`),
+        `# Headless Tree Sample: ${sampleName}\n\nTo run this example:\n\n- \`npm install\` to install dependencies\n- \`npm start\` to start the dev server\n\n`
+        + `You can run this sample from [Stackblitz](${stackblitz}) or [CodeSandbox](${codesandbox}). The source code is available on [GitHub](${github}).\n\n`,
+        { encoding: "utf8" }
+    )
+}
