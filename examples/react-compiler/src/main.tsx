144e4c195390d20486a010d1afcdacdcb4312f3b
Lukas Bach <contact@lukasbach.com>
2025-04-26 18:46:16 +0200

    react compiler example

---
+++ examples/react-compiler/src/main.tsx
@@ -0,0 +1,163 @@
+import { Fragment, StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+import {
+  DragTarget,
+  ItemInstance,
+  asyncDataLoaderFeature,
+  createOnDropHandler,
+  dragAndDropFeature,
+  hotkeysCoreFeature,
+  insertItemsAtTarget,
+  keyboardDragAndDropFeature,
+  removeItemsFromParents,
+  renamingFeature,
+  searchFeature,
+  selectionFeature,
+} from "@headless-tree/core";
+import { AssistiveTreeDescription, useTree } from "@headless-tree/react";
+import cx from "classnames";
+import { DemoItem, asyncDataLoader, data } from "./data";
+
+let newItemId = 0;
+const insertNewItem = (dataTransfer: DataTransfer) => {
+  const newId = `new-${newItemId++}`;
+  data[newId] = {
+    name: dataTransfer.getData("text/plain"),
+  };
+  return newId;
+};
+
+const onDropForeignDragObject = (
+  dataTransfer: DataTransfer,
+  target: DragTarget<DemoItem>,
+) => {
+  const newId = insertNewItem(dataTransfer);
+  insertItemsAtTarget([newId], target, (item, newChildrenIds) => {
+    data[item.getId()].children = newChildrenIds;
+  });
+};
+const onCompleteForeignDrop = (items: ItemInstance<DemoItem>[]) =>
+  removeItemsFromParents(items, (item, newChildren) => {
+    item.getItemData().children = newChildren;
+  });
+const onRename = (item: ItemInstance<DemoItem>, value: string) => {
+  data[item.getId()].name = value;
+};
+const getCssClass = (item: ItemInstance<DemoItem>) =>
+  cx("treeitem", {
+    focused: item.isFocused(),
+    expanded: item.isExpanded(),
+    selected: item.isSelected(),
+    folder: item.isFolder(),
+    drop: item.isDragTarget(),
+    searchmatch: item.isMatchingSearch(),
+  });
+
+export const Tree = () => {
+  const tree = useTree<DemoItem>({
+    initialState: {
+      expandedItems: ["fruit"],
+      selectedItems: ["banana", "orange"],
+    },
+    rootItemId: "root",
+    getItemName: (item) => item.getItemData()?.name,
+    isItemFolder: (item) => !!item.getItemData()?.children,
+    canReorder: true,
+    onDrop: createOnDropHandler((item, newChildren) => {
+      data[item.getId()].children = newChildren;
+    }),
+    onRename,
+    onDropForeignDragObject,
+    onCompleteForeignDrop,
+    createForeignDragObject: (items) => ({
+      format: "text/plain",
+      data: items.map((item) => item.getId()).join(","),
+    }),
+    canDropForeignDragObject: (_, target) => target.item.isFolder(),
+    indent: 20,
+    dataLoader: asyncDataLoader,
+    features: [
+      asyncDataLoaderFeature,
+      selectionFeature,
+      hotkeysCoreFeature,
+      dragAndDropFeature,
+      keyboardDragAndDropFeature,
+      renamingFeature,
+      searchFeature,
+    ],
+  });
+
+  return (
+    <>
+      {tree.isSearchOpen() && (
+        <div className="searchbox">
+          <input {...tree.getSearchInputElementProps()} />
+          <span>({tree.getSearchMatchingItems().length} matches)</span>
+        </div>
+      )}
+      <div {...tree.getContainerProps()} className="tree">
+        <AssistiveTreeDescription tree={tree} />
+        {tree.getItems().map((item) => (
+          <Fragment key={item.getId()}>
+            {item.isRenaming() ? (
+              <div
+                className="renaming-item"
+                style={{ marginLeft: `${item.getItemMeta().level * 20}px` }}
+              >
+                <input {...item.getRenameInputProps()} />
+              </div>
+            ) : (
+              <button
+                {...item.getProps()}
+                style={{ paddingLeft: `${item.getItemMeta().level * 20}px` }}
+              >
+                <div className={getCssClass(item)}>{item.getItemName()}</div>
+              </button>
+            )}
+          </Fragment>
+        ))}
+        <div style={tree.getDragLineStyle()} className="dragline" />
+      </div>
+
+      <div className="actionbar">
+        <div
+          className="foreign-dragsource"
+          draggable
+          onDragStart={(e) => {
+            e.dataTransfer.setData("text/plain", "hello world");
+          }}
+        >
+          Drag me into the tree!
+        </div>
+        <div
+          className="foreign-dropzone"
+          onDrop={(e) => {
+            alert(JSON.stringify(e.dataTransfer.getData("text/plain")));
+            console.log(e.dataTransfer.getData("text/plain"));
+          }}
+          onDragOver={(e) => e.preventDefault()}
+        >
+          Drop items here!
+        </div>
+        <button className="actionbtn" onClick={() => tree.openSearch()}>
+          Search items
+        </button>
+        <button
+          className="actionbtn"
+          onClick={() => tree.getItemInstance("fruit").startRenaming()}
+        >
+          Rename Fruit
+        </button>
+      </div>
+    </>
+  );
+};
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <div style={{ maxWidth: "300px" }}>
+      <Tree />
+    </div>
+  </StrictMode>,
+);
