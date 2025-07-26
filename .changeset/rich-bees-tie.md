32e71e946e229d55c28f8a3abcdfe4e2292b7067
Lukas Bach <contact@lukasbach.com>
2025-07-26 15:06:43 +0200

    feat: add workaround for react 17

---
+++ .changeset/rich-bees-tie.md
@@ -0,0 +1,5 @@
+---
+"@headless-tree/react": patch
+---
+
+Added optional @headless-tree/react/react17 import for useTree for compatibility
---
+++ packages/react/src/react17/index.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { TreeConfig, TreeState, createTree } from "@headless-tree/core";
+
+export const useTree = <T,>(config: TreeConfig<T>) => {
+  const [tree] = useState(() => ({ current: createTree(config) }));
+  const [state, setState] = useState<Partial<TreeState<T>>>(() =>
+    tree.current.getState(),
+  );
+
+  useEffect(() => {
+    tree.current.rebuildTree();
+  }, [tree]); // runs only once after mount
+
+  tree.current.setConfig((prev) => ({
+    ...prev,
+    ...config,
+    state: {
+      // ...prev.state,
+      ...state,
+      ...config.state,
+    },
+    setState: (state) => {
+      setState(state);
+      config.setState?.(state);
+    },
+  }));
+
+  return tree.current;
+};
