b9605da648fb66b90c437689074502a5c6522952
Lukas Bach <contact@lukasbach.com>
2025-09-19 19:22:47 +0200

    Revert "chore: remove console-logs"
    This reverts commit a559fc97ca397719ac079232dd7fdeda178ef77d.

--- packages/react/src/use-tree.tsx
+++ packages/react/src/use-tree.tsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import { TreeConfig, TreeState, createTree } from "@headless-tree/core";
 
-let mounted = false;
-
 export const useTree = <T,>(config: TreeConfig<T>) => {
   const [tree] = useState(() => ({ current: createTree(config) }));
   const [state, setState] = useState<Partial<TreeState<T>>>(() =>
@@ -10,17 +8,12 @@ export const useTree = <T,>(config: TreeConfig<T>) => {
   );
 
   useEffect(() => {
-    // (tree.current as any).setMounted(true);
+    (tree.current as any).setMounted(true);
     tree.current.rebuildTree();
-    setInterval(() => {
-      mounted = !mounted;
-      (tree.current as any).setMounted(mounted);
-      console.log({ mounted });
-    }, 3000);
-    // return () => {
-    //  // eslint-disable-next-line react-hooks/exhaustive-deps
-    //  (tree.current as any).setMounted(false);
-    // };
+    return () => {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      (tree.current as any).setMounted(false);
+    };
   }, [tree]);
 
   tree.current.setConfig((prev) => ({
