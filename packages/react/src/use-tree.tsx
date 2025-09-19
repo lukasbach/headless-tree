a559fc97ca397719ac079232dd7fdeda178ef77d
Lukas Bach <lukas.bach@goto.com>
2025-09-19 15:09:19 +0200

    chore: remove console-logs

--- packages/react/src/use-tree.tsx
+++ packages/react/src/use-tree.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { TreeConfig, TreeState, createTree } from "@headless-tree/core";
 
+let mounted = false;
+
 export const useTree = <T,>(config: TreeConfig<T>) => {
   const [tree] = useState(() => ({ current: createTree(config) }));
   const [state, setState] = useState<Partial<TreeState<T>>>(() =>
@@ -8,12 +10,17 @@ export const useTree = <T,>(config: TreeConfig<T>) => {
   );
 
   useEffect(() => {
-    (tree.current as any).setMounted(true);
+    // (tree.current as any).setMounted(true);
     tree.current.rebuildTree();
-    return () => {
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      (tree.current as any).setMounted(false);
-    };
+    setInterval(() => {
+      mounted = !mounted;
+      (tree.current as any).setMounted(mounted);
+      console.log({ mounted });
+    }, 3000);
+    // return () => {
+    //  // eslint-disable-next-line react-hooks/exhaustive-deps
+    //  (tree.current as any).setMounted(false);
+    // };
   }, [tree]);
 
   tree.current.setConfig((prev) => ({
